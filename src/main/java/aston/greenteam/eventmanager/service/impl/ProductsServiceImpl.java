36a6ccfc504e76ba3783245f629a89f031d80c4a
VITALY <9004673@mail.ru>
2023-06-26 18:43:31 +0300

    Products branch. Entity.

---
+++ src/main/java/aston/greenteam/eventmanager/service/impl/ProductsServiceImpl.java
@@ -0,0 +1,8 @@
+package aston.greenteam.eventmanager.service.impl;
+
+import aston.greenteam.eventmanager.service.ProductsService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductsServiceImpl implements ProductsService {
+}

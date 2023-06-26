ebfc6e08bce889d80dd0416e81e8983b76408c15
AnnLuciole <annluciole@mail.ru@>
2023-06-26 20:39:10 +0500

    Added right implementation for NoticeServiceImpl

--- src/main/java/aston/greenteam/eventmanager/service/impl/NoticeServiceImpl.java
+++ src/main/java/aston/greenteam/eventmanager/service/impl/NoticeServiceImpl.java
@@ -1,7 +1,8 @@
 package aston.greenteam.eventmanager.service.impl;
 
+import aston.greenteam.eventmanager.service.NoticeService;
 import org.springframework.stereotype.Service;
 
 @Service
-public class NoticeServiceImpl {
+public class NoticeServiceImpl implements NoticeService {
 }

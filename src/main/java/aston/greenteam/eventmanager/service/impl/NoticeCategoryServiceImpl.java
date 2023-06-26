60bdddbda97f68a1a05860c9dfc793255693d03d
AnnLuciole <annluciole@mail.ru@>
2023-06-26 20:28:15 +0500

    Added right annotation for NoticeCategoryServiceImpl

--- src/main/java/aston/greenteam/eventmanager/service/impl/NoticeCategoryServiceImpl.java
+++ src/main/java/aston/greenteam/eventmanager/service/impl/NoticeCategoryServiceImpl.java
@@ -1,8 +1,8 @@
 package aston.greenteam.eventmanager.service.impl;
 
 import aston.greenteam.eventmanager.service.NoticeCategoryService;
-import io.swagger.v3.oas.annotations.servers.Server;
+import org.springframework.stereotype.Service;
 
-@Server
+@Service
 public class NoticeCategoryServiceImpl implements NoticeCategoryService {
 }

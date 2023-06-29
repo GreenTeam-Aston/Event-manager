c18931d214eae9da6ede1510a09f380f31bf91c1
Kashevar128 <steve128@yandex.ru>
2023-06-29 23:48:37 +0300

    revert

---
+++ src/main/java/aston/greenteam/eventmanager/exceptions/NotFoundException.java
@@ -0,0 +1,8 @@
+package aston.greenteam.eventmanager.exceptions;
+
+public class NotFoundException extends RuntimeException{
+    @Override
+    public String getMessage() {
+        return super.getMessage();
+    }
+}

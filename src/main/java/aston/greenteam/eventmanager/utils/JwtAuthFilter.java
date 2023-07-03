e1abd29c5972440dbfb7b4c3787049bda052664b
PeterWosne <pwosne@gmail.com>
2023-07-03 22:11:10 +0300

    new dev 2.0

--- src/main/java/aston/greenteam/eventmanager/utils/JwtAuthFilter.java
+++
@@ -1,9 +0,0 @@
-package aston.greenteam.eventmanager.utils;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
-@Component
-public class JwtAuthFilter {
-
-}

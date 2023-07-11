c9a19476b076e0eb4a6768a8fedc92306b6ddeb2
Igor Shmidt <schmidt.igor2012@yandex.ru>
2023-07-11 17:05:08 +0300

    fix: устранить недостатки при merge

--- src/main/java/aston/greenteam/eventmanager/services/EventPhotoService.java
+++ src/main/java/aston/greenteam/eventmanager/services/EventPhotoService.java
@@ -1,12 +1,8 @@
 package aston.greenteam.eventmanager.services;
 
 import aston.greenteam.eventmanager.dtos.EventPhotoPostDto;
-import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.FileSystemResource;
-import org.springframework.core.io.Resource;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.util.List;
 
 public interface EventPhotoService {
     FileSystemResource getPhoto(Long photoId);
